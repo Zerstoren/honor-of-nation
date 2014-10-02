69c8acc8a915955579fdd035852479f14e41220b
Zerst <viacheslav.lucenko@mitridat.kiev.ua>
2014-10-03 01:09:28 +0300

    Complete

---
+++ public/js/model/dummy.js
@@ -0,0 +1,9 @@
+(function () {
+    'use strict';
+
+    define('model/dummy', [], function () {
+        return AbstractModel.extend({
+            model_url: '-'
+        });
+    });
+}());
--- public/js/view/block/map/footer.js
+++ public/js/view/block/map/footer.js
@@ -1,13 +1,29 @@
-define('view/block/map/footer', [], function() {
+define('view/block/map/footer', [
+    'service/standalone/map/gameMapItems/init',
+    'model/dummy'
+], function(
+    map,
+    ModelDummy
+) {
     'use strict';
 
     return AbstractView.extend({
         initialize: function (holder) {
             this.holder = holder;
+            this.template = this.getTemplate('block/map/footer');
+            this.data.footer = new ModelDummy();
+
+            this.initRactive();
         },
 
         render: function () {
-            this.holder.append(this.getTemplate('block/map/footer'));
+            this.holder.append(this.$el);
+            map.on('mouseMove', this.onMouseMove, this);
+        },
+
+        onMouseMove: function (coordinate) {
+            this.data.footer.set('x', coordinate[0]);
+            this.data.footer.set('y', coordinate[1]);
         }
     });
 });
