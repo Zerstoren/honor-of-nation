45fccee0723925401ce6808df456bc154bc3e899
Zerstoren <zerst.netpeak@gmail.com>
2014-11-23 17:51:54 +0200

    Fuck yeah

--- application/tests/selenium/Admin/UserTest.py
+++ application/tests/selenium/Admin/UserTest.py
@@ -5,41 +5,41 @@ class Selenium_Admin_UserTest(generic.Selenium_Admin_Generic):
         self.byCssSelector('.resources-edit input.' + name).clear()
         self.byCssSelector('.resources-edit input.' + name).send_keys(value)
 
-    # def testSetResources(self):
-    #     user = self.login()
-    #     self._goToAdmin()
-    #
-    #     self.byAttribute('data-type', 'player').click()
-    #     self.byCssSelector('.search-user-login').send_keys(user.getLogin())
-    #     self.byCssSelector('.search-user').click()
-    #
-    #     self.waitForElement('.resources-edit')
-    #
-    #     self._setResource('rubins', 5)
-    #     self._setResource('wood', 10)
-    #     self._setResource('steel', 50)
-    #     self._setResource('stone', 100)
-    #     self._setResource('eat', 200)
-    #     self._setResource('gold', 500)
-    #
-    #     self.byCssSelector('.save-info').click()
-    #
-    #     self.operationIsSuccess()
-    #
-    #     self.byCssSelector('.admin .close').click()
-    #     print(self.executeCommand("return require('system/socket').logsend"))
-    #
-    #     self.assertDictEqual(
-    #         self.getResources(),
-    #         {
-    #             'rubins': 5,
-    #             'wood': 10,
-    #             'steel': 50,
-    #             'stone': 100,
-    #             'eat': 200,
-    #             'gold': 500
-    #         }
-    #     )
+    def testSetResources(self):
+        user = self.login()
+        self._goToAdmin()
+
+        self.byAttribute('data-type', 'player').click()
+        self.byCssSelector('.search-user-login').send_keys(user.getLogin())
+        self.byCssSelector('.search-user').click()
+
+        self.waitForElement('.resources-edit')
+
+        self._setResource('rubins', 5)
+        self._setResource('wood', 10)
+        self._setResource('steel', 50)
+        self._setResource('stone', 100)
+        self._setResource('eat', 200)
+        self._setResource('gold', 500)
+
+        self.byCssSelector('.save-info').click()
+
+        self.operationIsSuccess()
+
+        self.byCssSelector('.admin .close').click()
+        print(self.executeCommand("return require('system/socket').logsend"))
+
+        self.assertDictEqual(
+            self.getResources(),
+            {
+                'rubins': 5,
+                'wood': 10,
+                'steel': 50,
+                'stone': 100,
+                'eat': 200,
+                'gold': 500
+            }
+        )
 
 
     def testCreateTerrainByCoordinate_WrongUser(self):
