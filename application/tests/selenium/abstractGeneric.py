b7ae8a8906948bbf1432815d16de0d2da10fba7d
Zerstoren <zerst.netpeak@gmail.com>
2014-11-25 21:39:33 +0200

    s

--- application/tests/selenium/abstractGeneric.py
+++ application/tests/selenium/abstractGeneric.py
@@ -89,7 +89,9 @@ class Selenium_Abstract_Generic(Generic):
             self.closeWindow('ALL')
 
             self.managedProcess.send_signal(signal.SIGINT)
-            self.managedProcess.wait()
+            self.managedProcess.communicate()
+            print(self.managedProcess.returncode)
+
             self.managedProcess = None
 
     def _executeTestPart(self, function, outcome, isTest=False):
