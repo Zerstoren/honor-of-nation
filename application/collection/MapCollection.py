c0539b4c75cb58aad60c7c7df1ecf332d926ca01
Zerstoren <zerst.netpeak@gmail.com>
2015-03-07 00:39:10 +0200

    asd

--- application/collection/MapCollection.py
+++ application/collection/MapCollection.py
@@ -1,10 +1,9 @@
 from . import Abstract
 
 import service.MapUserVisible
-from models.Map.Domain import Map_Domain
+
 
 class Map_Collection(Abstract.AbstractCollection):
-    domain = Map_Domain
 
     def getMapVisible(self, user):
         result = []
