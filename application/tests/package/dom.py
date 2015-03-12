f4cac16c24916c42e0aa0e5d7a5f2c8c05d03d7f
Viacheslav Lucenko <viacheslav.lucenko@trdata.com>
2015-03-12 14:29:36 +0200

    r

--- application/tests/package/dom.py
+++ application/tests/package/dom.py
@@ -10,6 +10,8 @@ class Dom(abstract.AbstractDeclaration):
     def value(self, element, value):
         element.clear()
         self.sleep(0.6)
+        print(element.get_attribute('value'))
         assert element.get_attribute('value') == "" or element.get_attribute('value') == "0"
         element.send_keys(str(value))
+        print(element.get_attribute('value'))
         assert element.get_attribute('value') == str(value)
